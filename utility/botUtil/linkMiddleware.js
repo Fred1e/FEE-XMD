c129ef15c91f9e4c4dd0e4a0cd58cb160c069077
Fredi AI <frediezra60@gmail.com>
2026-01-22 23:25:11 +0300

    Update linkMiddleware.js

--- utility/botUtil/linkMiddleware.js
+++ utility/botUtil/linkMiddleware.js
@@ -2,15 +2,15 @@ module.exports = async (context, next) => {
     const { m, isBotAdmin } = context;
 
     if (!m.isGroup) {
-        return m.reply(`╭┈┈┈┈━━━━━━┈┈┈┈◈
-┋❒ This command only works in groups! 
-╰┈┈┈┈━━━━━━┈┈┈┈◈`);
+        return m.reply(`❥┈┈┈┈┈┈┈┈┈┈┈┈┈┈➤
+┋✿ This command only works in groups! 
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈➤`);
     }
     
     if (!isBotAdmin) {
-        return m.reply(`╭┈┈┈┈━━━━━━┈┈┈┈◈
-┋❒ I need admin rights to get the group link! 
-╰┈┈┈┈━━━━━━┈┈┈┈◈`);
+        return m.reply(`┈┈┈┈┈┈┈┈┈┈┈┈┈┈➤
+┋✿ I need admin rights to get the group link! 
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈➤`);
     }
 
     await next(); // Proceed to the next function (main handler)
